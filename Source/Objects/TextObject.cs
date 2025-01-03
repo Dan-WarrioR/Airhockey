b0fab326a9f4e2be8d4825bf7ef7683ddd58c5e3
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-03 16:10:17 +0100

    Created separated text object for score

---
+++ Source/Objects/TextObject.cs
@@ -0,0 +1,43 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Source.Objects
+{
+	public class TextObject : GameObject
+	{
+		private static readonly Color TextColor = Color.White;
+
+		private const string FontPath = @"C:\Windows\Fonts\Arial.ttf";
+		private const uint CharacterSize = 24;
+
+		public override Vector2f Position => _text.Position;
+
+		private Text _text;
+
+		public TextObject(Vector2f initialPosition, string text) : base(initialPosition)
+		{
+			var font = new Font(FontPath);
+
+			_text = new(text, font, CharacterSize)
+			{
+				FillColor = TextColor,
+				Position = initialPosition,
+			};
+		}
+
+		protected override void ChangePosition(Vector2f position)
+		{
+			_text.Position = position;
+		}
+
+		public void ChangeText(string text)
+		{
+			_text.DisplayedString = text;
+		}
+
+		public override void Draw(RenderTarget target, RenderStates states)
+		{
+			_text.Draw(target, states);
+		}
+	}
+}
